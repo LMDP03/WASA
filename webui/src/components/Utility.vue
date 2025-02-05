702f7946e43e49093ce77b93e23077c8b013c898
LMDP03 <dellapenna.2041721@studenti.uniroma1.it>
2025-02-05 17:03:51 +0100

    recent updates

---
+++ webui/src/components/Utility.vue
@@ -0,0 +1,47 @@
+<script>
+import {RouterLink} from 'vue-router';
+
+export default {
+    props: {
+        show: boolean,
+        users: Array,
+        title: String,
+    },
+    data() {
+        return {
+
+            error: "",
+
+            searchName: "",
+
+            searchnameValid: new RegExp('ˆ.*?$'),
+
+            filteredUsers: [],
+        };        
+    },
+    methods: {
+        close() {
+            this.searchName = "";
+            window.location.reload();
+            this.$emit('close');
+        },
+        async filterUsers() {
+            this.error = "";
+
+            this.filteredUsers = this.users;
+            if (this.searchName.length < 3 || this.searchName.length > 16 || this.searchnameValid.test(this.searchName)) {
+                this.error = "Invalid search name.";
+                this.filteredUsers = [];
+                return;
+            }
+            if (this.title == "search") {
+                try {
+                    const url = '/users/${sessionStorage.userId}/others?';
+
+                }
+            }
+        }
+    }
+
+},
+</script>
