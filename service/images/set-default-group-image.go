3176ce4af27a39668426f96aeb9f8b6ff96a2e16
LMDP03 <dellapenna.2041721@studenti.uniroma1.it>
2025-02-09 01:31:01 +0100

    modified path

--- service/images/set-default-group-image.go
+++ service/images/set-default-group-image.go
@@ -3,5 +3,5 @@ package images
 import "fmt"
 
 func SetDefaultGroupImage(convId int) string {
-	return fmt.Sprintf("./storage/conversations/%d/chat_image.jpg", convId)
+	return fmt.Sprintf("./storage/groups/%d/chat_image.jpg", convId)
 }
